01c0fad7a83688a27c28bd5940d81861efdb35c8
Jonathan Wafellman <jwafellman@gmail.com>
2017-05-27 15:14:31 +0200

    process babel transform and add eslint

--- webpack-server.config.js
+++ webpack-server.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/server/server.js',
@@ -6,7 +7,15 @@ module.exports = {
     filename: 'server.bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  externals: {
-    express: 'express',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ]
   },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
 };
