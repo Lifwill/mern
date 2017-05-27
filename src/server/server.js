95b1f9414eb9483178934f87b91ca0a896258098
Jonathan Wafellman <jwafellman@gmail.com>
2017-05-27 23:04:35 +0200

    fis the compile script, add the mongoose in the server and add user model copy/pasted from other source

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import serverConfig from './serverConfig';
+
 const app = express();
+mongoose.connect(serverConfig.mongoURL, (error) => {
+  if (error) {
+    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+    throw error;
+  }
+
+  // feed some dummy data in DB.
+  //dummyData();
+});
 
 app.use('/public', express.static('dist/public'));
 app.get('/', function (req, res) {
@@ -15,5 +27,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!'); // eslint-disable-line no-console
 });
---
+++ src/server/serverConfig.js
@@ -0,0 +1,5 @@
+const config = {
+  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern',
+};
+
+export default config;
